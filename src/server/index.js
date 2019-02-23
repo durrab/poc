d8a2df3d3225a7b0d09226a617403ba2c76c351a
Durrab Khan <durrab@gmail.com>
2019-02-22 18:22:19 -0800

    config changes

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const os = require('os');
+var conf = require("config");
+
+const express = require("express");
+const os = require("os");
 
 const app = express();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.use(express.static("dist"));
+app.get("/api/getUsername", (req, res) =>
+  res.send({ username: os.userInfo().username })
+);
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.listen(conf.get("port") || 8080, () =>
+  console.log(`Listening on port ${conf.get("port") || 8080}!`)
+);
